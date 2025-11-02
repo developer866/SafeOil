ab3af147b845d17a5c3dabe26458e312063c56ee
Developer866 <opeyemijoseph866@gmail.com>
2025-11-02 15:08:20 +0100

    Refactor social media icons layout and styling in App component for improved design consistency

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,35 @@ import Contact from "./Components/Pages/Contact";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Pages/Footer";
 import ScrollToTop from "./Components/ScrollTop";
+
 function App() {
   return (
     <main className="md:max-w-9xl mx-auto">
       <Navbar />
-          <div className="md:right-50 right-5  z-30 md:bottom-30 bottom-30 fixed text-white">
-          
-          <div className="flex">
-            <img src="/Whatsapp.jpeg" alt="whatsapp" className="w-20" />
-       
-            <img src="/facebook.jpeg" alt="whatsapp" className="w-20" />
-          </div>
-        </div>
+
+      {/* Floating social media icons */}
+      <div className="fixed right-5 bottom-5 z-30 flex gap-2">
+        <img
+          src="/Whatsapp.jpeg"
+          alt="whatsapp"
+          className="w-16 rounded-full"
+        />
+        <img
+          src="/facebook.jpeg"
+          alt="facebook"
+          className="w-16 rounded-full"
+        />
+      </div>
+
+      <ScrollToTop />
       <Routes>
-        <ScrollToTop />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+
       <Footer />
     </main>
   );
